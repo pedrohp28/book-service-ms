cb22c2c34ab2939f48ecc972377cf7e2ef1f3754
pedrohp28 <pedrohp36@gmail.com>
2025-12-23 14:11:22 -0300

    update proxy

--- src/main/java/br/com/erudio/controller/BookController.java
+++ src/main/java/br/com/erudio/controller/BookController.java
@@ -42,75 +42,10 @@ public class BookController {
 
         Exchange exchange = proxy.getExchange(book.getPrice(), "USD", currency);
 
-        book.setEnvironment(port + " FEIGN");
+        //book.setEnvironment(port + " FEIGN");
+        book.setEnvironment("BOOK PORT: " + port + " EXCHANGE " + exchange.getEnvironment());
         book.setPrice(exchange.getConvertedValue());
         book.setCurrency(currency);
         return book;
     }
-
-    /*
-    // http://localhost:8100/book-service/1/BRL
-    @GetMapping(value = "/{id}/{currency}",
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public Book findBook(
-            @PathVariable("id") Long id,
-            @PathVariable("currency") String currency
-    ){
-        String port = informationService.retrieveServerPort();
-
-        var book = repository.findById(id).orElseThrow();
-
-        HashMap<String, String> params = new HashMap<>();
-        params.put("amount", book.getPrice().toString());
-        params.put("from", "USD");
-        params.put("to", currency);
-
-        var response = new RestTemplate()
-            .getForEntity("http://localhost:8000/exchange-service/" +
-                "{amount}/{from}/{to}", Exchange.class, params);
-
-        Exchange exchange = response.getBody();
-
-        book.setEnvironment(port);
-        book.setPrice(exchange.getConvertedValue());
-        book.setCurrency(currency);
-        return book;
-    }
-
-    // http://localhost:8100/book-service/1/BRL
-    @GetMapping(value = "/{id}/{currency}",
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public Book findBook(
-            @PathVariable("id") Long id,
-            @PathVariable("currency") String currency
-    ){
-        String port = informationService.retrieveServerPort();
-
-        var book = repository.findById(id).orElseThrow();
-
-        book.setEnvironment(port);
-        book.setCurrency(currency);
-        return book;
-    }
-
-    // http://localhost:8100/book-service/1/BRL
-    @GetMapping(value = "/{id}/{currency}",
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public Book findBook(
-            @PathVariable("id") Long id,
-            @PathVariable("currency") String currency
-    ){
-        String port = informationService.retrieveServerPort();
-
-        return new Book(
-            1L,
-            "Nigel Poulton",
-            "Docker Deep Dive",
-            new Date(),
-            15.8,
-            "BRL",
-            port
-        );
-    }
-*/
 }
