596d3953c9ba0896e949074764af39ddf0ffafc1
pedrohp28 <pedrohp36@gmail.com>
2025-12-24 13:24:46 -0300

    - Implementando springdoc com openApi; - Configurando cors - Configurando eureka para pegar hostname correto (WSL)

--- src/main/java/br/com/erudio/controller/BookController.java
+++ src/main/java/br/com/erudio/controller/BookController.java
@@ -5,19 +5,18 @@ import br.com.erudio.environment.InstanceInformationService;
 import br.com.erudio.model.Book;
 import br.com.erudio.proxy.ExchangeProxy;
 import br.com.erudio.repository.BookRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.Date;
-import java.util.HashMap;
 
 @RestController
 @RequestMapping("book-service")
+@Tag(name = "Book Endpoint")
 public class BookController {
 
     @Autowired
@@ -29,7 +28,7 @@ public class BookController {
     @Autowired
     private ExchangeProxy proxy;
 
-    // http://localhost:8100/book-service/1/BRL
+    @Operation(summary = "Find a specific book by your ID")
     @GetMapping(value = "/{id}/{currency}",
             produces = MediaType.APPLICATION_JSON_VALUE)
     public Book findBook(
--- src/main/java/br/com/erudio/controller/FooBarController.java
+++ src/main/java/br/com/erudio/controller/FooBarController.java
@@ -4,6 +4,7 @@ import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("book-service")
+@Tag(name = "Foo Bar Endpoint")
 public class FooBarController {
 
     private Logger logger = LoggerFactory.getLogger(FooBarController.class);
